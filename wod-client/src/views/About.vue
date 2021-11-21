2c0702cc9127fb74eba4b3ba3b267af39f7ff436
Hiroshi Hamano <hiroshihamano1996@gmail.com>
2021-11-21 12:00:03 +0100

    About page adapted, add env path before pip in Dockerfile

--- wod-client/src/views/About.vue
+++ wod-client/src/views/About.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="about">
-    <h1>Welcome to Webcam Face Recognition Demo</h1>
-    <h2>Usage:</h2>
-    <ol>
-      <li>Allow the web application to access to your webcam</li>
-      <li>Click on take photos to take photos (total 5 images in every 2 seconds)</li>
-      <li>The taken photos are displayed in a gallery on the bottom</li>
-      <li>If all 5 photos are taken you can send it to execute face detection on every image</li>
-      <li>Finally images with corresponding boundary box around faces are return and displayed</li>
-    </ol>
-  </div>
+  <b-container class="container">
+    <b-row class="about mb-3" align-h="center">
+      <h1>Welcome to Webcam Face Recognition Demo</h1>
+    </b-row>
+    <b-row class="usage mb-3" align-h="center">
+      <h2>Usage:</h2>
+    </b-row>
+    <b-row class="steps" align-h="center">
+      <h3>1. Allow the web application to access to your webcam</h3>
+      <h3>2. Click on take photos to take photos (total 5 images in every 2 seconds)</h3>
+      <h3>3. The taken photos are displayed in a gallery on the bottom</h3>
+      <h3>4. If all 5 photos are taken you can send it to execute face detection on every image</h3>
+      <h3>5. Finally images with corresponding boxes around faces are return and displayed</h3>
+    </b-row>
+  </b-container>
 </template>
